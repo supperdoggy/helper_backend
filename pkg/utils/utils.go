9f1a83ba7c999c9cf76b6502e3c63ae64f84fbcb
Maksym Marchyshak <maksym.marchyshak@icloud.com>
2023-03-06 22:57:10 +0200

    changed token value

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,13 +1,27 @@
 package utils
 
 import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"log"
 	"math/rand"
 	"time"
 
+	"golang.org/x/crypto/bcrypt"
 	"gopkg.in/night-codes/types.v1"
 )
 
+// GenerateToken returns a unique token based on the provided email string
 func GenerateToken() string {
 	rand.Seed(time.Now().UnixNano())
-	return types.String(rand.Int())
+	hash, err := bcrypt.GenerateFromPassword([]byte(types.String(rand.Int())), bcrypt.DefaultCost)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Hash to store:", string(hash))
+
+	hasher := md5.New()
+	hasher.Write(hash)
+	return hex.EncodeToString(hasher.Sum(nil))
 }
